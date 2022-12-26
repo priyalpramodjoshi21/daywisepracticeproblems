1cf5ed2935ced6b06cd0a9eadddcf691e601a14e
“priyalpramodjoshi21” <priyalpramodjoshi@gmail.com>
2022-12-26 08:37:21 -0800

    Added:Sum and Avg

---
+++ Day5/SumAverage.sh
@@ -0,0 +1,22 @@
+i1=$((RANDOM%100))
+echo $i1
+i2=$((RANDOM%100))
+echo $i2
+i3=$((RANDOM%100))
+echo $i3
+i4=$((RANDOM%100))
+echo $i4
+i5=$((RANDOM%100))
+echo $i5
+i4=$(($i2+$i3))
+echo $sum
+i1=$((sum/2))
+echo $avg
+
+
+
+
+
+
+
+
