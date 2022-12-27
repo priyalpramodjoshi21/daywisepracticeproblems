6738003c25d7539afb7124e6f2349bc2ab8f79aa
“priyalpramodjoshi21” <priyalpramodjoshi@gmail.com>
2022-12-27 04:16:15 -0800

    Addded: days printing

---
+++ Day5/days.sh
@@ -0,0 +1,13 @@
+echo "Enter a number"
+read n
+case $n in
+1) echo "sunday" ;;
+2) echo "monday" ;;
+3) echo "tuesday" ;;
+4) echo "wenesday" ;;
+5) echo "thrusday" ;;
+6) echo "friday" ;;
+7) echo "saturday" ;;
+*) echo "Enter number between 1 to 7" ;;
+esac
+
